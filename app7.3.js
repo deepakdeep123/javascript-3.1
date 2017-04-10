bfaca04518ef4717058c46ec5658a005231006aa
deepakdeep123 <bhattacharya.deep@gmail.com>
2017-04-10 18:07:29 +0530

    Add files via upload

---
+++ app7.3.js
@@ -0,0 +1,9 @@
+ var a = 1;          
+            function increase(){
+
+
+               document.getElementById("text").value=a;
+                a=a+1;
+                console.log("a")
+
+            }            
